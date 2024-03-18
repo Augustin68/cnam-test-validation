8710887455ca918976ae104e57b245f3e3d91030
Raffael <maxou4170@gmail.com>
2024-03-18 21:52:43 +0100

    init TP3

---
+++ TP3/loanApp/loanApp/LoanCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loanApp
+{
+    internal class LoanCalculator
+    {
+        float ComputeLoanMonthlyPayment(float capital, float annualRate, int monthDuration)
+        {
+            float loan = 0;
+            return loan;
+        }
+    }
+}
